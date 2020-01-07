7a80386c469527b4d7cdf4084df918a088abc2c2
moksleivis <sdgf@dvf.lt>
2020-01-07 13:47:25 +0200

    part two

--- exersize/exercises.py
+++ exersize/exercises.py
@@ -28,6 +28,7 @@ def execute_select_query(query):
         connection.close()
 
 
+# Part One
 def exercise_one():
     # 1. Write a query to display the names (first_name, last_name)
     # and salary for all employees whose salary
@@ -105,3 +106,98 @@ exercise_six()
 exercise_seven()
 
 
+# Part Two
+def exercise_1():
+    # 1. Write a query to list the amount of jobs available in the employees table. ANSWER: 19.
+    query = """SELECT COUNT(DISTINCT job_id)
+                FROM employees"""
+    print(execute_select_query(query))
+
+
+def exercise_2():
+    # 2. Write a query to get the total of salaries to pay.
+    query = """SELECT SUM(salary)
+                FROM employees"""
+    print(execute_select_query(query))
+
+
+def exercise_3():
+    # 3. Write a query to get the minimum salary from employees table.
+    query = """SELECT salary
+                FROM employees ORDER BY salary LIMIT 1"""
+    print(execute_select_query(query))
+
+
+def exercise_4():
+    # 4. Write a query to get the highest salary of an employee.
+    query = """SELECT salary
+                FROM employees ORDER BY salary DESC LIMIT 1"""
+    print(execute_select_query(query))
+
+
+def exercise_5():
+    # 5. Write a query to get average salary and number of employees working  in department 90.
+    query = """SELECT AVG(salary), COUNT(employee_id)
+                FROM employees 
+                WHERE department_id = 90"""
+    print(execute_select_query(query))
+
+
+def exercise_6():
+    # 6. Write a query to get the highest, lowest, sum and average salary of all employees.
+    query = """SELECT MAX(salary), MIN(salary), SUM(salary), AVG(salary)
+                FROM employees"""
+    print(execute_select_query(query))
+
+
+def exercise_7():
+    # 7. Write a query to get the number of employees with the same job.
+    query = """SELECT COUNT(employee_id)
+                FROM employees
+                GROUP BY job_id"""
+    print(execute_select_query(query))
+
+
+def exercise_8():
+    # 8. Write a query to get the difference between the highest and lowest salaries.
+    query = """SELECT MAX(salary) - MIN(salary)
+                FROM employees"""
+    print(execute_select_query(query))
+
+
+def exercise_9():
+    # 9. Write a query to get the department ID and the total salary payable in each department.
+    query = """SELECT department_id, SUM(salary)
+                FROM employees
+                GROUP BY department_id"""
+    print(execute_select_query(query))
+
+
+def exercise_10():
+    # 10. Write a query to get the average salary for each job ID excluding programmers.
+    query = """SELECT AVG(salary)
+                FROM employees
+                WHERE NOT job_id = 'IT_PROG'
+                GROUP BY job_id"""
+    print(execute_select_query(query))
+
+
+def exercise_11():
+    # 11. Write a query to find the manager ID and the salary of the lowest paid employee for that manager.
+    query = """SELECT manager_id, MIN(salary)
+                FROM employees
+                GROUP BY manager_id"""
+    print(execute_select_query(query))
+
+
+exercise_1()
+exercise_2()
+exercise_3()
+exercise_4()
+exercise_5()
+exercise_6()
+exercise_7()
+exercise_8()
+exercise_9()
+exercise_10()
+exercise_11()
